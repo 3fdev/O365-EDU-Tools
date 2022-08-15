89d57b015cadd3518b84a95911f70f465418a8bc
3fdev <92921179+3fdev@users.noreply.github.com>
2022-08-15 11:34:57 +0100

    Add files via upload

---
+++ Test_OU Creation.ps1
@@ -0,0 +1,24 @@
+﻿strart-transript
+
+Install Module
+
+Connect to Active Directory
+
+Import-Module ActiveDirectory
+
+$credential = Get-Credential
+
+Get-Aduser -Server "cloudpoint.ie" -Filter * -Credential $credential[12:56] Allan Drummy
+
+Get-ADForest -Server "cloudpoint.ie"
+
+New-ADOrganizationalUnit -Server "cloudpoint.ie" -Name "Thomas" -Path "DC=Cloudpoint,DC=ie" -Credential $credential
+
+Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
+
+import-module ActiveDirectory
+
+$credential = Get-Credential
+
+New-ADOrganizationalUnit -Server "cloudpoint.ie" -Name "Thomas" -Path "DC=Cloudpoint,DC=ie" -Credential $credential
+
